ffb6dbdfd63f696093c1e57cc05a263ad3fc0d44
Rafal Wanjas <wanjasrafal@gmail.com>
2020-03-01 21:17:38 +0100

    Add nullable modifier to `scAddress`
    This allows to pass null which iss acceptable and expected value.

--- app/src/main/java/com/raphau/licho/data/Message.kt
+++ app/src/main/java/com/raphau/licho/data/Message.kt
@@ -2,11 +2,11 @@ package com.raphau.licho.data
 
 abstract class Message {
     abstract val destinationAddress: String
-    abstract val scAddress: String
+    abstract val scAddress: String?
 }
 
 data class SmsMessage(
     override val destinationAddress: String,
-    override val scAddress: String,
+    override val scAddress: String?,
     val text: String
     ) : Message()
