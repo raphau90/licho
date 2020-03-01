778c1aad69763a3ba7a033962e3f936e152ce658
Rafal Wanjas <wanjasrafal@gmail.com>
2020-03-01 20:53:55 +0100

    Add send basic text message functionality
    This commit extends `MessagesRepository` class with method to send regular text based sms message.

--- app/src/main/java/com/raphau/licho/MessagesRepository.kt
+++ app/src/main/java/com/raphau/licho/MessagesRepository.kt
@@ -2,7 +2,20 @@ package com.raphau.licho
 
 import android.content.Context
 import android.telephony.SmsManager
+import com.raphau.licho.data.Message
+import com.raphau.licho.data.SmsMessage
 
 class MessagesRepository(context: Context) {
     private val smsManager = SmsManager.getDefault()
+
+    fun sendMessage(message: Message) {
+        when (message) {
+            is SmsMessage -> smsManager.sendTextMessage(
+                message.destinationAddress,
+                message.scAddress,
+                message.text,
+                null,
+                null)
+        }
+    }
 }
