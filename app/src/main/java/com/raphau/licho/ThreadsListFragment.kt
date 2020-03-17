e5d8e85bd2944788dbb34a1e16d9316a9eb06b8d
Rafal Wanjas <wanjasrafal@gmail.com>
2020-03-17 13:25:12 +0100

    Add simple list of threads
    This commit adds bare minimum implementation of threads_list_row.xml
    layout and simple implementation to ThreadsListRecyclerAdapter to
    present the threads.

--- app/src/main/java/com/raphau/licho/ThreadsListFragment.kt
+++ app/src/main/java/com/raphau/licho/ThreadsListFragment.kt
@@ -5,10 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.lifecycle.observe
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.raphau.licho.data.MessageThread
+import com.raphau.licho.data.SmsMessage
 import com.raphau.licho.di.InjectableFragment
 import com.raphau.licho.viewmodel.ThreadsListViewModel
 import kotlinx.android.synthetic.main.fragment_threads_list.*
@@ -31,9 +34,10 @@ class ThreadsListFragment : InjectableFragment() {
 
     override fun onInjected() {
         threads_list_recycler_view.adapter = threadsAdapter
+        threads_list_recycler_view.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false)
         viewModel.getMessages().observe(this) {
             threadsAdapter.threads = it
-
+            threadsAdapter.notifyDataSetChanged()
         }
     }
 
@@ -47,18 +51,32 @@ class ThreadsListFragment : InjectableFragment() {
         var threads: List<MessageThread> = ArrayList()
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ThreadViewHolder {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.threads_list_row, parent, false)
+            return ThreadViewHolder(view)
         }
 
-        override fun getItemCount(): Int {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        }
+        override fun getItemCount() = threads.size
 
         override fun onBindViewHolder(holder: ThreadViewHolder, position: Int) {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            holder.setItem(threads[position])
         }
 
-        class ThreadViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        class ThreadViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+            val avatar = view.findViewById<ImageView>(R.id.avatar)
+            val address = view.findViewById<TextView>(R.id.address)
+            val content = view.findViewById<TextView>(R.id.content)
+            val date = view.findViewById<TextView>(R.id.date)
+
+            fun setItem(thread: MessageThread) {
+                //TODO: set avatar
+                address.text = thread.address
+                val message = thread.messages.first()
+                when (message) {
+                    is SmsMessage ->content.text = message.text
+                }
+                date.text = thread.lastMessageDate.toString()
+            }
 
         }
     }
