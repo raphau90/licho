8ba7e3f6f9d5afed95271a2049a226006e47aded
Rafal Wanjas <wanjasrafal@gmail.com>
2020-03-20 13:40:04 +0100

    Add displayname property to MessageThread
    This property is either contact's display name (if present) or address
    of the message

--- app/src/main/java/com/raphau/licho/data/MessageThread.kt
+++ app/src/main/java/com/raphau/licho/data/MessageThread.kt
@@ -6,8 +6,13 @@ data class MessageThread(
     val lastMessageDate: Long
 ) {
     var contact: Contact? = null
+    set(value) {
+        field = value
+        displayName = value?.displayName ?: address
+    }
     val messages: List<Message> = ArrayList()
     var isRead = true
+    var displayName = address
 
     fun add(message: Message) {
         (messages as ArrayList).add(message)
